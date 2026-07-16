49bbe350a872ad97328e6e0344baaf7919c170ba
student apnacollge <student@apnacollge.in>
2026-07-17 00:44:04 +0530

    feat: worker process with job execution + exit code handling

---
+++ src/core/worker.js
@@ -0,0 +1,79 @@
+const { exec } = require('child_process');
+const Queue = require('./queue');
+const { calculateBackoff } = require('./backoff');
+const ConfigManager = require('../config/configManager');
+const { v4: uuidv4 } = require('uuid');
+
+class Worker {
+  constructor(queue = new Queue()) {
+    this.workerId = uuidv4();
+    this.queue = queue;
+    this.isRunning = false;
+    this.currentJobProcess = null;
+    this.pollInterval = 1000;
+    this.configManager = new ConfigManager(queue.store);
+  }
+
+  async start() {
+    this.isRunning = true;
+    await this.loop();
+  }
+
+  stop() {
+    this.isRunning = false;
+    // Note: We don't kill currentJobProcess to allow graceful shutdown
+  }
+
+  async loop() {
+    while (this.isRunning) {
+      try {
+        const job = this.queue.acquire(this.workerId);
+        
+        if (job) {
+          await this.processJob(job);
+        } else {
+          // Wait before polling again
+          await new Promise(resolve => setTimeout(resolve, this.pollInterval));
+        }
+      } catch (err) {
+        console.error(`[Worker ${this.workerId}] Error: ${err.message}`);
+        await new Promise(resolve => setTimeout(resolve, this.pollInterval));
+      }
+    }
+  }
+
+  processJob(job) {
+    return new Promise((resolve) => {
+      this.currentJobProcess = exec(job.command, (error, stdout, stderr) => {
+        this.currentJobProcess = null;
+        
+        if (error) {
+          this.handleFailure(job).then(resolve);
+        } else {
+          this.queue.complete(job.id);
+          resolve();
+        }
+      });
+    });
+  }
+
+  async handleFailure(job) {
+    const newState = this.queue.fail(job.id);
+    
+    if (newState === 'failed') {
+      const base = parseInt(this.configManager.get('backoff-base', 2), 10);
+      const delayMs = calculateBackoff(job.attempts, base);
+      
+      // Schedule transition back to pending
+      setTimeout(() => {
+        try {
+          this.queue.retryFailed(job.id);
+        } catch (err) {
+          // Job might have been modified or deleted
+        }
+      }, delayMs);
+    }
+  }
+}
+
+module.exports = Worker;
